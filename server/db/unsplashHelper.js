4439ceddfe716f22a907241d4ffbb0ae1bafd730
Amar <amar.vadhia@gmail.com>
2019-07-23 16:32:29 -0700

    Update gitIgnore file

--- server/db/unsplashHelper.js
+++ server/db/unsplashHelper.js
@@ -1,10 +1,10 @@
-
+const APIkey = require('../../unsplashAPI');
 const axios = require('axios');
 
 module.exports = {
   getImages: (query, cb) => {
     const url = `https://api.unsplash.com/search/photos?query=${query}&page=1&per_page=30&orientation=landscape`;
-    axios.get(url, { headers: { Authorization: 'Client-ID 71f41bb88ae6fc4b5f052fa3034a6b224d4e5b5c76cc355ab2f6957d4ef05834' } })
+    axios.get(url, { headers: { Authorization: `Client-ID ${APIkey}`  } })
       .then((response) => {
         cb(null, response.data);
       })
